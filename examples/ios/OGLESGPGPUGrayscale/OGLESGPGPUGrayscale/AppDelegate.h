8be8fceabccafdfc376045bc2ae4d0afed7f863b
Markus Konrad <post@mkonrad.net>
2014-10-16 14:15:22 +0200

    added empty project for iOS

---
+++ examples/ios/OGLESGPGPUGrayscale/OGLESGPGPUGrayscale/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OGLESGPGPUGrayscale
+//
+//  Created by Markus Konrad on 16.10.14.
+//  Copyright (c) 2014 INKA Research Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ examples/ios/OGLESGPGPUGrayscale/OGLESGPGPUGrayscale/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OGLESGPGPUGrayscale
+//
+//  Created by Markus Konrad on 16.10.14.
+//  Copyright (c) 2014 INKA Research Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
