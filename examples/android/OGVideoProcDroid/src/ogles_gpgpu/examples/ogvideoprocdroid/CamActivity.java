50e8d622efa2c295662c44a9ddc2873999114894
Markus Konrad <post@mkonrad.net>
2015-01-05 16:15:25 +0100

    seems as camera frame is received via GL texture

--- examples/android/OGVideoProcDroid/src/ogles_gpgpu/examples/ogvideoprocdroid/CamActivity.java
+++ examples/android/OGVideoProcDroid/src/ogles_gpgpu/examples/ogvideoprocdroid/CamActivity.java
@@ -1,18 +1,28 @@
 package ogles_gpgpu.examples.ogvideoprocdroid;
 
+import java.io.IOException;
+
 import ogles_gpgpu.OGJNIWrapper;
 
 import com.android.grafika.gles.EglCore;
 import com.android.grafika.gles.WindowSurface;
 
 import android.app.Activity;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.hardware.Camera.Size;
+import android.opengl.GLES11Ext;
+import android.opengl.GLES20;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class CamActivity extends Activity implements SurfaceHolder.Callback {
+public class CamActivity extends Activity implements SurfaceHolder.Callback, SurfaceTexture.OnFrameAvailableListener {
 	private final static String TAG = "CamActivity";
+	
+	private final static int CAM_FPS = 30;
+	private final static int CAM_FRAME_TEXTURE_TARGET = GLES11Ext.GL_TEXTURE_EXTERNAL_OES;
 
 	// The holder for our SurfaceView.  The Surface can outlive the Activity (e.g. when
     // the screen is turned off and back on with the power button).
@@ -27,7 +37,13 @@ public class CamActivity extends Activity implements SurfaceHolder.Callback {
     
     private WindowSurface windowSurface;
     
-
+    private Camera cam;
+    private Size camPreviewFrameSize;
+    private Object camLock = new Object();
+    private SurfaceTexture camTexture;
+    private int camTextureId;
+    
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,12 +60,16 @@ public class CamActivity extends Activity implements SurfaceHolder.Callback {
     protected void onResume() {
         Log.i(TAG, "onResume");
         super.onResume();
+        
+        startCam();
     }
 
     @Override
     protected void onPause() {
         Log.i(TAG, "onPause");
         super.onPause();
+        
+        stopCam();
     }
 
 	@Override
@@ -63,13 +83,32 @@ public class CamActivity extends Activity implements SurfaceHolder.Callback {
 		
 		windowSurface = new WindowSurface(eglCore, holder.getSurface(), false);
 		windowSurface.makeCurrent();
+		
+		Log.i(TAG, "opening camera device");
+		startCam();
+		
+		camTextureId = createGLTexture();
+		camTexture = new SurfaceTexture(camTextureId);
+		camTexture.setOnFrameAvailableListener(this);
+		
+		Log.i(TAG, "created camera frame texture with id " + camTextureId);
+		
+		Log.i(TAG, "starting camera preview");
+		
+		try {
+			cam.setPreviewTexture(camTexture);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		cam.startPreview();
 	}
-	
+
 	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 		Log.i(TAG, "surface changed to size " + width + "x" + height);
 		
-		ogWrapper.prepare(width, height);
+		ogWrapper.prepare(camPreviewFrameSize.width, camPreviewFrameSize.height);
 	}
 
 	@Override
@@ -78,7 +117,98 @@ public class CamActivity extends Activity implements SurfaceHolder.Callback {
 		
 		surfaceHolder = null;
 		
+		releaseGL();
+		
 		eglCore.release();
 	}
+	
+	@Override
+	public void onFrameAvailable(SurfaceTexture surfaceTexture) {
+		camTexture.updateTexImage();
+		
+//		Log.i(TAG, "updated camera frame texture");
+		
+		// ogWrapper calls...
+	}
 
+	private void releaseGL() {
+		if (windowSurface != null) {
+			windowSurface.release();
+			windowSurface = null;
+		}
+		
+		eglCore.makeNothingCurrent();
+	}
+
+	private void startCam() {
+		if (cam != null) return;
+		
+		Log.i(TAG, "starting camera");
+		
+		synchronized (camLock) {
+			Camera.CameraInfo camInfo = new Camera.CameraInfo();
+			
+			int numCameras = Camera.getNumberOfCameras();
+			for (int i = 0; i < numCameras; i++) {
+                Camera.getCameraInfo(i, camInfo);
+                if (camInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                    cam = Camera.open(i);
+                    break;
+                }
+            }
+            if (cam == null) {
+                Log.i(TAG, "No front-facing camera found; opening default");
+                cam = Camera.open();    // opens first back-facing camera
+            }
+            if (cam == null) {
+                throw new RuntimeException("Unable to open camera");
+            }
+            
+            Camera.Parameters camParams = cam.getParameters();
+            camPreviewFrameSize = camParams.getPreferredPreviewSizeForVideo();
+            camParams.setPreviewSize(camPreviewFrameSize.width, camPreviewFrameSize.height);
+            
+            camParams.setPreviewFpsRange(CAM_FPS * 1000, CAM_FPS * 1000);
+            camParams.setRecordingHint(true);
+            cam.setParameters(camParams);
+            
+            int[] fps = new int[2];
+            camParams.getPreviewFpsRange(fps);
+            
+            Log.i(TAG, "camera preview frame size is " + camPreviewFrameSize.width + "x" + camPreviewFrameSize.height);
+            Log.i(TAG, "camera preview fps range is " + fps[0] / 1000.0f + " - " + fps[1] / 1000.0f);
+		}
+	}
+	
+	private void stopCam() {
+		if (cam == null) return;
+		
+		synchronized (camLock) {
+			cam.stopPreview();
+			cam.release();
+			cam = null;
+		}
+		
+		Log.i(TAG, "stopped camera");
+	}
+	
+	private int createGLTexture() {
+		int[] textures = new int[1];
+        GLES20.glGenTextures(1, textures, 0);
+
+        int texId = textures[0];
+        
+        if (texId <= 0) {
+        	throw new RuntimeException("invalid GL texture id generated");
+        }
+        
+        GLES20.glBindTexture(CAM_FRAME_TEXTURE_TARGET, texId);
+
+        GLES20.glTexParameterf(CAM_FRAME_TEXTURE_TARGET, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameterf(CAM_FRAME_TEXTURE_TARGET, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameteri(CAM_FRAME_TEXTURE_TARGET, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(CAM_FRAME_TEXTURE_TARGET, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+
+        return texId;
+	}
 }
