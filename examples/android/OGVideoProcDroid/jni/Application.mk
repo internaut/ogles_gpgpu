8d057e98fc734928035c63c1a89019d50647e958
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-01-22 04:32:52 +0700

    Cleanup
    Remove DOS EOL. Fix warning "No newline at end of file".

--- examples/android/OGVideoProcDroid/jni/Application.mk
+++ examples/android/OGVideoProcDroid/jni/Application.mk
@@ -4,5 +4,5 @@ APP_CPPFLAGS := -frtti -fexceptions
 
 # Build all available ABIs
 APP_ABI := all
-# What platform (API level) to build against
+# What platform (API level) to build against
 APP_PLATFORM := android-14
