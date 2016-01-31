1c627c383d6117f57e8b719480c9abbf1073f918
David Hirvonen <dhirvonen@elucideye.com>
2016-01-31 17:35:12 -0500

    added default texture wrap

--- ogles_gpgpu/common/gl/fbo.cpp
+++ ogles_gpgpu/common/gl/fbo.cpp
@@ -88,6 +88,10 @@ void FBO::createAttachedTex(int w, int h, bool genMipmap, GLenum attachment, GLe
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     }
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    
     Tools::checkGLErr("FBO" , "fbo texture stuff");
 
     // bind it to FBO
