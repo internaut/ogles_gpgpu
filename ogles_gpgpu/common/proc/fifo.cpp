e4cf001b17c6924569a1e299ddb742426300c482
David Hirvonen <dhirvonen@elucideye.com>
2016-02-28 14:46:34 -0500

    Functional FIFO texture buffer

--- ogles_gpgpu/common/proc/fifo.cpp
+++ ogles_gpgpu/common/proc/fifo.cpp
@@ -13,57 +13,228 @@
 using namespace std;
 using namespace ogles_gpgpu;
 
-const char *FIFOProc::fshaderFIFOSrc = OG_TO_STR(
+class NoopProc : public ogles_gpgpu::FilterProcBase {
+public:
+    NoopProc(float gain=1.f) : gain(gain) {}
+    virtual const char *getProcName() { return "NoopProc"; }
+private:
+    virtual const char *getFragmentShaderSource() { return fshaderNoopSrc; }
+    virtual void getUniforms() { shParamUGain = shader->getParam(UNIF, "gain"); }
+    virtual void setUniforms() { glUniform1f(shParamUGain, gain); }
+    static const char *fshaderNoopSrc; // fragment shader source
+    float gain = 1.f;
+    GLint shParamUGain;
+};
 
+const char * NoopProc::fshaderNoopSrc = OG_TO_STR
+(
 #if defined(OGLES_GPGPU_OPENGLES)
-precision mediump float;
+ precision mediump float;
 #endif
+ varying vec2 vTexCoord;
+ uniform sampler2D uInputTex;
+ uniform float gain;
+ void main()
+ {
+     vec4 val = texture2D(uInputTex, vTexCoord);
+     gl_FragColor = clamp(val * gain, 0.0, 1.0);
+ });
 
-uniform sampler2D uInputTex;
-varying vec2 vTexCoord;
+// #################### FIFO ####################
 
-void main()
-{
-    gl_FragColor = texture2D(uInputTex, vTexCoord)
-}
-);
-
-FIFOProc::FIFOProc()
-{
-
-}
-
-void FIFOProc::setUniforms()
-{
-
+GLuint FifoProc::getTexId(int index) const {
+    assert(index >= 0 && index < procPasses.size());
+    return procPasses[index]->getOutputTexId();
 }
 
-void FIFOProc::getUniforms()
-{
-
+int FifoProc::getIn() const {
+    return m_inputIndex;
 }
 
-int FIFOProc::init(int inW, int inH, unsigned int order, bool prepareForExternalInput)
-{
-    // Create FBO to store the previous texture
-    prevFBO = std::make_shared<FBO>();
-    prevFBO->setGLTexUnit(1);
-    
-    return FilterProcBase::init(inW, inH, order, prepareForExternalInput);
+int FifoProc::getOut() const {
+    return m_inputIndex;
 }
 
-void FIFOProc::render()
-{
-    OG_LOGINF(getProcName(), "input tex %d, target %d, framebuffer of size %dx%d", texId, texTarget, outFrameW, outFrameH);
-    filterRenderPrepare();
+FifoProc::FifoProc(int size) {
+    for(int i = 0; i < size; i++) {
+        procPasses.push_back( new NoopProc );
+    }
+}
+
+FifoProc::~FifoProc() {
+    // remove all pass instances
+    for(auto &it : procPasses) {
+        delete it;
+    }
     
-    setUniforms();
+    procPasses.clear();
+}
+
+#pragma mark ProcInterface methods
+
+int FifoProc::init(int inW, int inH, unsigned int order, bool prepareForExternalInput) {
     
-    Tools::checkGLErr(getProcName(), "render prepare");
+    m_inputIndex = m_outputIndex = m_count = 0;
     
-    filterRenderSetCoords(); //   if (fbo) fbo->bind();
-    Tools::checkGLErr(getProcName(), "render set coords");
+    int num = 0;
+    for(auto &it : procPasses) {
+        num += it->init(inW, inH, num, prepareForExternalInput);
+    }
+    return num;
+}
+
+int FifoProc::reinit(int inW, int inH, bool prepareForExternalInput) {
     
-    filterRenderCleanup();
-    Tools::checkGLErr(getProcName(), "render cleanup");
+    m_inputIndex = m_outputIndex = m_count = 0;
+    
+    int num = 0;
+    for(auto &it : procPasses) {
+        num += it->reinit(inW, inH, prepareForExternalInput);
+    }
+    return num;
+}
+
+void FifoProc::cleanup() {
+    for(auto &it : procPasses) {
+        it->cleanup();
+    }
+}
+
+void FifoProc::setExternalInputDataFormat(GLenum fmt) {
+    // Render into input FBO
+    assert(m_inputIndex >= 0 && m_inputIndex < procPasses.size());
+    return procPasses[m_inputIndex]->setExternalInputDataFormat(fmt);
+}
+
+void FifoProc::setExternalInputData(const unsigned char *data) {
+    // Render into input FBO
+    assert(m_inputIndex >= 0 && m_inputIndex < procPasses.size());
+    return procPasses[m_inputIndex]->setExternalInputData(data);
+}
+
+void FifoProc::createFBOTex(bool genMipmap) {
+    for(auto &it : procPasses) {
+        it->createFBOTex(genMipmap);
+    }
+}
+
+// 0 : [0][ ][ ]
+//     [I][ ][ ]
+//     [O][ ][ ]
+//
+// 1 : [0][1][ ]
+//     [ ][I][ ]
+//     [O][ ][ ]
+//
+// 2 : [0][1][2]
+//     [ ][ ][I]
+//     [O][ ][ ]
+//
+// 3 : [3][1][2]
+//     [I][ ][ ]
+//     [ ][O][ ]
+//
+// 4 : [3][4][2]
+//     [ ][I][ ]
+//     [ ][ ][O]
+//
+// 5 : [3][4][5]
+//     [O][ ][ ]
+//     [ ][ ][I]
+
+void FifoProc::render() {
+    // Render into input FBO
+    assert(m_inputIndex >= 0 && m_inputIndex < procPasses.size());
+    procPasses[m_inputIndex]->render();
+    
+    m_inputIndex = (m_inputIndex + 1) % procPasses.size();
+    if(m_count == procPasses.size()) {
+        m_outputIndex = (m_outputIndex + 1) % procPasses.size();
+    }
+    m_count = std::min(m_count + 1, int(procPasses.size()));
+}
+
+void FifoProc::printInfo() {
+    OG_LOGINF(getProcName(), "begin info for %u passes", (unsigned int)procPasses.size());
+    for(auto &it : procPasses) {
+        it->printInfo();
+    }
+    OG_LOGINF(getProcName(), "end info");
+}
+
+void FifoProc::useTexture(GLuint id, GLuint useTexUnit, GLenum target) {
+    assert(m_inputIndex >= 0 && m_inputIndex < procPasses.size());
+    return procPasses[m_inputIndex] ->useTexture(id, useTexUnit, target);
+}
+
+GLuint FifoProc::getTextureUnit() const {
+    assert(m_inputIndex >= 0 && m_inputIndex < procPasses.size());
+    return procPasses[m_inputIndex] ->getTextureUnit();
+}
+
+void FifoProc::setOutputSize(float scaleFactor) {
+    for(auto &it : procPasses) {
+        it->setOutputSize(scaleFactor);
+    }
+}
+
+void FifoProc::setOutputSize(int outW, int outH) {
+    for(auto &it : procPasses) {
+        it->setOutputSize(outW, outH);
+    }
+}
+
+int FifoProc::getOutFrameW() const {
+    assert(m_outputIndex >= 0 && m_outputIndex < procPasses.size());
+    return procPasses[m_outputIndex]->getOutFrameW(); // any will do
+}
+
+int FifoProc::getOutFrameH() const {
+    assert(m_outputIndex >= 0 && m_outputIndex < procPasses.size());
+    return procPasses[m_outputIndex]->getOutFrameH();
+}
+
+int FifoProc::getInFrameW() const {
+    assert(m_inputIndex >= 0 && m_inputIndex < procPasses.size());
+    return procPasses[m_inputIndex]->getInFrameW(); // any will do
+}
+
+int FifoProc::getInFrameH() const {
+    assert(m_inputIndex >= 0 && m_inputIndex < procPasses.size());
+    return procPasses[m_inputIndex]->getInFrameH(); // any will do
+}
+
+bool FifoProc::getWillDownscale() const {
+    assert(m_inputIndex >= 0 && m_inputIndex < procPasses.size());
+    return procPasses[m_inputIndex]->getWillDownscale(); // any will do
+}
+
+void FifoProc::getResultData(unsigned char *data) const {
+    assert(m_outputIndex >= 0 && m_outputIndex < procPasses.size());
+    return procPasses[m_outputIndex]->getResultData(data);
+}
+
+void FifoProc::getResultData(FrameDelegate &delegate) const {
+    assert(m_outputIndex >= 0 && m_outputIndex < procPasses.size());
+    return procPasses[m_outputIndex]->getResultData(delegate);
+}
+
+MemTransfer *FifoProc::getMemTransferObj() const {
+    assert(m_outputIndex >= 0 && m_outputIndex < procPasses.size());
+    return procPasses[m_outputIndex]->getMemTransferObj();
+}
+
+MemTransfer *FifoProc::getInputMemTransferObj() const {
+    assert(m_inputIndex >= 0 && m_inputIndex < procPasses.size());
+    return procPasses[m_inputIndex]->getMemTransferObj();
+}
+
+GLuint FifoProc::getInputTexId() const {
+    assert(m_inputIndex >= 0 && m_inputIndex < procPasses.size());
+    return procPasses[m_inputIndex]->getInputTexId();
+}
+
+GLuint FifoProc::getOutputTexId() const {
+    assert(m_outputIndex >= 0 && m_outputIndex < procPasses.size());
+    return procPasses[m_inputIndex]->getOutputTexId();
 }
--- ogles_gpgpu/common/proc/fifo.h
+++ ogles_gpgpu/common/proc/fifo.h
@@ -18,55 +18,196 @@
 #include "gl/fbo.h"
 
 namespace ogles_gpgpu {
-
-/**
- * GPGPU FIFO texture buffer
- */
-class FIFOProc : public FilterProcBase {
+    
+class FifoProc : public ProcInterface {
 public:
+    
+    FifoProc(int size=2);
+    
     /**
-     * Constructor.
+     * Deconstructor.
+     * Removes all pass instances.
      */
-    FIFOProc();
-
+    virtual ~FifoProc();
+    
+    /**
+     * Init the processor for input frames of size <inW>x<inH> which is at
+     * position <order> in the processing pipeline.
+     * Abstract method.
+     */
+    virtual int init(int inW, int inH, unsigned int order, bool prepareForExternalInput = false);
+    
+    /**
+     * Reinitialize the proc for a different input frame size of <inW>x<inH>.
+     */
+    virtual int reinit(int inW, int inH, bool prepareForExternalInput = false);
+    
     /**
      * Return the processors name.
      */
     virtual const char *getProcName() {
-        return "FIFOProc";
+        return "FifoProc";
     }
     
     /**
-     * render
+     * Cleanup processor's resources.
+     */
+    virtual void cleanup();
+    
+    /**
+     * Set pixel data format for input data to <fmt>. Must be set before init() / reinit().
+     */
+    virtual void setExternalInputDataFormat(GLenum fmt);
+    
+    /**
+     * Insert external data into this processor. It will be used as input texture.
+     * Note: init() must have been called with prepareForExternalInput = true for that.
+     */
+    virtual void setExternalInputData(const unsigned char *data);
+    
+    /**
+     * Create a texture that is attached to the FBO and will contain the processing result.
+     * Set <genMipmap> to true to generate a mipmap (usually only works with POT textures).
+     */
+    virtual void createFBOTex(bool genMipmap);
+    
+    /**
+     * Render a result, i.e. run the shader on the input texture.
+     * Abstract method.
      */
     virtual void render();
     
     /**
-     * init
-     */    
-    virtual int init(int inW, int inH, unsigned int order, bool prepareForExternalInput);
-    
-private:
+     * Print some information about the processor's setup.
+     */
+    virtual void printInfo();
     
     /**
-     * Get the fragment shader source.
+     * Use texture id <id> as input texture at texture <useTexUnit> with texture target <target>.
      */
-    virtual const char *getFragmentShaderSource() { return fshaderFIFOSrc; }
+    virtual void useTexture(GLuint id, GLuint useTexUnit = 1, GLenum target = GL_TEXTURE_2D);
     
     /**
-     * Set additional uniforms.
+     * Return used texture unit.
      */
-    virtual void setUniforms();
+    virtual GLuint getTextureUnit() const;
     
     /**
-     * Get uniform indices.
+     * Set output size by scaling down or up the input frame size by factor <scaleFactor>.
      */
-    virtual void getUniforms();
-
-    static const char *fshaderFIFOSrc;  // fragment shader source
-
-    std::shared_ptr<FBO> prevFBO;
+    virtual void setOutputSize(float scaleFactor);
+    
+    /**
+     * Set output size by scaling down or up the input frame to size <outW>x<outH>.
+     */
+    virtual void setOutputSize(int outW, int outH);
+    
+    /**
+     * Set the render orientation to <o>.
+     * Not implemented for multipass processors!
+     */
+    virtual void setOutputRenderOrientation(RenderOrientation o) {
+        assert(false);
+    }
+    
+    /**
+     * Get the render orientation.
+     * Not implemented for multipass processors!
+     */
+    virtual RenderOrientation getOutputRenderOrientation() const {
+        return RenderOrientationNone;
+    }
+    
+    /**
+     * Get the output frame width.
+     */
+    virtual int getOutFrameW() const;
+    
+    /**
+     * Get the output frame height.
+     */
+    virtual int getOutFrameH() const;
+    
+    /**
+     * Get the input frame width.
+     */
+    virtual int getInFrameW() const;
+    
+    /**
+     * Get the input frame height.
+     */
+    virtual int getInFrameH() const;
+    
+    /**
+     * Returns true if output size < input size.
+     */
+    virtual bool getWillDownscale() const;
+    
+    /**
+     * Return the result data from the FBO.
+     */
+    virtual void getResultData(unsigned char *data) const;
+    
+    /**
+     * Apply callback to output FBO texture.
+     */
+    virtual void getResultData(FrameDelegate &delegate) const;
+    
+    /**
+     * Return pointer to MemTransfer object of this processor.
+     */
+    virtual MemTransfer *getMemTransferObj() const;
+    
+    /**
+     * Return pointer to the input MemTransfer object of this processor.
+     */
+    virtual MemTransfer *getInputMemTransferObj() const;
+    
+    /**
+     * Return input texture id.
+     */
+    virtual GLuint getInputTexId() const;
+    
+    /**
+     * Return the output texture id (= texture that is attached to the FBO).
+     */
+    virtual GLuint getOutputTexId() const;
+    
+    /**
+     * Get number of passes for this multipass processor.
+     */
+    size_t getNumPasses() const {
+        return procPasses.size();
+    }
+    
+    /**
+     * Return te list of processor instances of each pass of this multipass processor.
+     */
+    std::vector<ProcInterface *>& getProcPasses() { return procPasses; }
+    const std::vector<ProcInterface *>& getProcPasses() const { return procPasses; }
+    
+    size_t getBufferCount() { return m_count; }
+    
+    virtual int getIn() const;
+    virtual int getOut() const;
+    virtual GLuint getTexId(int index) const;
+    
+protected:
+    
+    int m_count = 0;
+    int m_inputIndex = -1;
+    int m_outputIndex = -1;
+    
+    /**
+     * This method should be called whenever <procPasses> changed.
+     */
+    void multiPassInit();
+    
+    std::vector<ProcInterface *> procPasses;   // holds all instances to the single processing passes. strong ref!
 };
+
+typedef FifoProc FIFOPRoc;
+    
 }
 
 #endif
